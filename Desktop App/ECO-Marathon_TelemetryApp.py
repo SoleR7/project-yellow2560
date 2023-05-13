a5a36b208b3accd0281139b25edf3d2195a0e2ce
SoleR7 <jordisolersierra@gmail.com>
2023-05-13 20:11:56 +0200

    Desktop App development 11

--- Desktop App/ECO-Marathon_TelemetryApp.py
+++ Desktop App/ECO-Marathon_TelemetryApp.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 import pandas as pd
 import csv
 from datetime import datetime, timedelta
@@ -13,11 +14,72 @@ from PyQt5.QtCore import Qt
 from numpy import number
 
 
+class speedGraph(QWidget):
+    def __init__(self, df):
+        super().__init__()
+        #self.setFixedSize(1600, 1000)
+        self.setWindowTitle("Speed Graph")
+
+        # Create a figure and a canvas to draw the plot on
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        # Plot the temperature data on the figure
+        ax = self.figure.add_subplot(111)
+        ax.plot(df['CurrentTime'].values, df['speed'].values * 1.852)
+        ax.set_title('Speed over Time')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Speed')
+
+        # Create a navigation toolbar and add it to the layout
+        toolbar = NavigationToolbar(self.canvas, self)
+        layout = QVBoxLayout()
+        layout.addWidget(toolbar)
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+        
+
+class circuitElevationGraph(QWidget):
+    def __init__(self, df):
+        super().__init__()
+
+        self.setWindowTitle("Elevation Analysis")
+
+        # Create a figure and a canvas to draw the plot on
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        # Plot the latitude and longitude data on a scatter plot
+        ax = self.figure.add_subplot(111)
+        scatter = ax.scatter(df['longitude'].values, df['latitude'].values, c=df['altitude'].values, cmap='viridis')
+        #ax.set_title('Latitude and Longitude')
+        #ax.set_xlabel('Longitude')
+        #ax.set_ylabel('Latitude')
+        cbar = self.figure.colorbar(scatter)
+        cbar.set_label('Altitude (meters)')
+
+        # Connect each point by its next one with a line plot
+        ax.plot(df['longitude'].values, df['latitude'].values, color='gray')
+
+        # Remove X and Y axis ticks and labels
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+
+        # Create a navigation toolbar and add it to the layout
+        toolbar = NavigationToolbar(self.canvas, self)
+        layout = QVBoxLayout()
+        layout.addWidget(toolbar)
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setFixedSize(650, 800)
+        self.setFixedSize(650, 850)
         self.csv_file_name = None
         self.selected_lap = 0
 
@@ -27,7 +89,7 @@ class MainWindow(QMainWindow):
         # logo
         self.logo_label = QLabel(self)
         pixmap = QPixmap('logo_equipo.png')
-        pixmap = pixmap.scaled(650, 800, Qt.KeepAspectRatio)
+        pixmap = pixmap.scaled(650, 850, Qt.KeepAspectRatio)
         self.logo_label.setPixmap(pixmap)
         self.logo_label.setGeometry(0, 0, pixmap.width(), pixmap.height())
 
@@ -62,29 +124,6 @@ class MainWindow(QMainWindow):
         self.cbLaps.addItems(["Lap 0"])
         self.cbLaps.currentIndexChanged.connect(self.update_lap_selected)
         self.cbLaps.setEnabled(False)
-
-        # buttons
-        self.buttons_title_label = QLabel('Graph Options')
-        
-        self.racingLineMap_button = QPushButton("Racing line")
-        self.racingLineMap_button.clicked.connect(self.racingLineMap_plot)
-        self.racingLineMap_button.setEnabled(False)
-
-        self.circuitElevation_button = QPushButton("Circuit Elevation")
-        self.circuitElevation_button.clicked.connect(self.circuitElevation_plot)
-        self.circuitElevation_button.setEnabled(False)
-
-        self.speed_button = QPushButton("Speed")
-        self.speed_button.clicked.connect(self.speed_plot)
-        self.speed_button.setEnabled(False)
-
-        self.accelerometer_button = QPushButton("Accelerometer")
-        self.accelerometer_button.clicked.connect(self.accelerometer_plot)
-        self.accelerometer_button.setEnabled(False)
-
-        self.temperature_button = QPushButton("Temperature")
-        self.temperature_button.clicked.connect(self.temperature_plot)
-        self.temperature_button.setEnabled(False)
         
         self.info_label_lapTime = QLabel("Laptime: ")
         self.info_label_lapTime.setEnabled(False)
@@ -94,8 +133,47 @@ class MainWindow(QMainWindow):
         
         self.info_label_temp_per_lap = QLabel("Avg temp: ")
         self.info_label_temp_per_lap.setEnabled(False)
+        
+        # buttons
+        self.buttons_title_label = QLabel('Graphs\t\t (Whole session \ per lap)')
+        
+        #  whole session
+        self.speed_button = QPushButton("Speed")
+        self.speed_button.clicked.connect(self.speed_plot)
+        self.speed_button.setEnabled(False)
+        
+        self.temperature_button = QPushButton("Temperature")
+        self.temperature_button.clicked.connect(self.temperature_plot)
+        self.temperature_button.setEnabled(False)
+        
+        self.accelerometer_button = QPushButton("Accelerometer")
+        self.accelerometer_button.clicked.connect(self.accelerometer_plot)
+        self.accelerometer_button.setEnabled(False)
+        
+        # per lap
+        self.racingLineMap_button = QPushButton("Racing line")
+        self.racingLineMap_button.clicked.connect(self.racingLineMap_plot)
+        self.racingLineMap_button.setEnabled(False)
+
+        self.circuitElevation_button = QPushButton("Elevation Analysis")
+        self.circuitElevation_button.clicked.connect(self.circuitElevation_plot)
+        self.circuitElevation_button.setEnabled(False)
+        
+        self.speed_perLap_button = QPushButton("Speed")
+        self.speed_perLap_button.clicked.connect(self.speed_perLap_plot)
+        self.speed_perLap_button.setEnabled(False)
+        
+        self.temp_perLap_button = QPushButton("Temperature")
+        self.temp_perLap_button.clicked.connect(self.temp_perLap_plot)
+        self.temp_perLap_button.setEnabled(False)
+        
+        self.accelerometer_perLap_button = QPushButton("Accelerometer")
+        self.accelerometer_perLap_button.clicked.connect(self.accelerometer_perLap_plot)
+        self.accelerometer_perLap_button.setEnabled(False)
+        
 
         # layouts
+        #(right, top, left, bottom)
         main_v_layout = QVBoxLayout()
         logo_h_layout = QHBoxLayout()
         csvTitle_h_layout = QHBoxLayout()
@@ -105,9 +183,9 @@ class MainWindow(QMainWindow):
         info_v_layout_left = QVBoxLayout()
         info_v_layout_right = QVBoxLayout()
         buttonTtile_h_layout = QHBoxLayout()
-        button_h_layout_one = QHBoxLayout()
-        button_h_layout_two = QHBoxLayout()
-        button_h_layout_three = QHBoxLayout()
+        button_h_main_layout = QHBoxLayout()
+        button_v_layout_left = QVBoxLayout()
+        button_v_layout_right = QVBoxLayout()
 
         main_v_layout.addLayout(logo_h_layout)
         main_v_layout.addLayout(csvTitle_h_layout)
@@ -115,25 +193,23 @@ class MainWindow(QMainWindow):
         main_v_layout.addLayout(infoTitle_h_layout)
         main_v_layout.addLayout(second_h_layout)
         main_v_layout.addLayout(buttonTtile_h_layout)
-        main_v_layout.addLayout(button_h_layout_one)
-        main_v_layout.addLayout(button_h_layout_two)
-        main_v_layout.addLayout(button_h_layout_three)
+        main_v_layout.addLayout(button_h_main_layout)
         
         logo_h_layout.addWidget(self.logo_label)
         
         csvTitle_h_layout.addWidget(self.open_csv_title)
-        csvTitle_h_layout.setContentsMargins(30, 30, 1, 1)
+        csvTitle_h_layout.setContentsMargins(30, 10, 1, 1)
 
         first_h_layout.addWidget(self.csv_file_name_label)
         first_h_layout.addWidget(self.csv_button)
-        first_h_layout.setContentsMargins(100, 10, 110, 50)
+        first_h_layout.setContentsMargins(100, 1, 110, 1)
         
         infoTitle_h_layout.addWidget(self.info_label_title)
         infoTitle_h_layout.setContentsMargins(30, 1, 1, 1)
         
         second_h_layout.addLayout(info_v_layout_left)
         second_h_layout.addLayout(info_v_layout_right)
-        second_h_layout.setContentsMargins(70, 10, 70, 50)
+        second_h_layout.setContentsMargins(70, 10, 70, 20)
         
         info_v_layout_left.addWidget(self.info_label_sessionTime)
         info_v_layout_left.addWidget(self.info_label_gpsSats)
@@ -146,21 +222,26 @@ class MainWindow(QMainWindow):
         info_v_layout_right.addWidget(self.info_label_lapTime)
         info_v_layout_right.addWidget(self.info_label_speed_per_lap)
         info_v_layout_right.addWidget(self.info_label_temp_per_lap)
-        info_v_layout_right.setContentsMargins(50, 0, 40, 0)
+        info_v_layout_right.setContentsMargins(50, 0, 0, 0)
 
         buttonTtile_h_layout.addWidget(self.buttons_title_label)
         buttonTtile_h_layout.setContentsMargins(30, 1, 1, 1)
         
-        button_h_layout_one.addWidget(self.racingLineMap_button)
-        button_h_layout_one.addWidget(self.circuitElevation_button)
-        button_h_layout_one.setContentsMargins(90, 15, 110, 5)
-
-        button_h_layout_two.addWidget(self.speed_button)
-        button_h_layout_two.addWidget(self.accelerometer_button)
-        button_h_layout_two.setContentsMargins(90, 1, 110, 5)
-
-        button_h_layout_three.addWidget(self.temperature_button)
-        button_h_layout_three.setContentsMargins(90, 1, 110, 50)
+        button_h_main_layout.addLayout(button_v_layout_left)
+        button_h_main_layout.addLayout(button_v_layout_right)
+        button_h_main_layout.setContentsMargins(70, 15, 70, 5)
+        
+        button_v_layout_left.addWidget(self.speed_button)
+        button_v_layout_left.addWidget(self.accelerometer_button)
+        button_v_layout_left.addWidget(self.temperature_button)
+        button_v_layout_left.setAlignment(Qt.AlignTop)
+        
+        button_v_layout_right.addWidget(self.racingLineMap_button)
+        button_v_layout_right.addWidget(self.circuitElevation_button)
+        button_v_layout_right.addWidget(self.speed_perLap_button)
+        button_v_layout_right.addWidget(self.accelerometer_perLap_button)
+        button_v_layout_right.addWidget(self.temp_perLap_button)
+        button_v_layout_right.setAlignment(Qt.AlignTop)
 
         # placeholder widget to hold the main layout.
         widget = QWidget()
@@ -178,16 +259,22 @@ class MainWindow(QMainWindow):
         if file_dialog.exec():
             # Get the name of the selected file and store it in self.csv_file_name
             self.csv_file_name = file_dialog.selectedFiles()[0]
-            print(self.csv_file_name)
 
             # check if the file is valid
             if self.is_valid_csv_file(self.csv_file_name, 11):
-                print("csv is valid!")
                 self.csv_file_name_label.setText(self.csv_file_name)
                 self.csv_file_name_label.setFont(QFont('Arial', 7))
                 
                 # extract info from csv file
                 self.csv_info = self.extract_csv_info(self.csv_file_name, 11)
+                
+                # Read the CSV file and create a pandas DataFrame.
+                self.df = pd.read_csv(self.csv_file_name,
+                                      names=['CurrentTime', 'satellites', 'speed', 'latitude', 'longitude', 'x', 'y',
+                                             'z', 'temperature', 'altitude', 'currentLap'])
+                
+                # Convert the CurrentTime column to a datetime object
+                self.df['CurrentTime'] = pd.to_datetime(self.df['CurrentTime'], format='%H:%M:%S')
                 
                 # update info section
                 self.info_label_sessionTime.setText("Session time: "+self.csv_info["elapsed_time"])
@@ -199,7 +286,7 @@ class MainWindow(QMainWindow):
                 self.info_label_laps.setText("Laps: " + str(self.csv_info["num_laps"]))
                 self.info_label_laps.setEnabled(True)
                 
-                self.info_label_speed.setText("Avg Speed: " + str(self.csv_info["avg_speed"]) + "km/h")
+                self.info_label_speed.setText("Avg Speed: " + str(self.csv_info["avg_speed"]) + " km/h")
                 self.info_label_speed.setEnabled(True)
                 
                 self.info_label_temp.setText("Avg Temp: " + str(self.csv_info["avg_temperature"]) + " ºC")
@@ -210,15 +297,20 @@ class MainWindow(QMainWindow):
                 
                 self.populate_CB_laps(self.csv_info["num_laps"])
                 self.cbLaps.setEnabled(True)
-
-                # Read the CSV file and create a pandas DataFrame.
-                self.df = pd.read_csv('LOG.CSV',
-                                      names=['CurrentTime', 'satellites', 'speed', 'latitude', 'longitude', 'x', 'y',
-                                             'z', 'temperature', 'altitude', 'currentLap'])
                 
-                # Convert the CurrentTime column to a datetime object
-                self.df['CurrentTime'] = pd.to_datetime(self.df['CurrentTime'], format='%H:%M:%S')
-
+                # extract info from lap 0
+                self.extract_csv_info_perLap(self.selected_lap, self.df)
+ 
+                #activate buttons
+                self.speed_button.setEnabled(True)
+                self.temperature_button.setEnabled(True)
+                self.accelerometer_button.setEnabled(True)
+                self.racingLineMap_button.setEnabled(True)
+                self.circuitElevation_button.setEnabled(True)
+                self.speed_perLap_button.setEnabled(True)
+                self.accelerometer_perLap_button.setEnabled(True)
+                self.temp_perLap_button.setEnabled(True)
+                
             else:
                 dlg = QMessageBox.warning(self, "Error", "Selected csv file is not valid!")
 
@@ -275,40 +367,74 @@ class MainWindow(QMainWindow):
             }
 
 
-    def update_lap_selected(self, index):
-        selected_lap = index
-        #ignore '-1'
-        #update the right-part info depending on the lap selected
-        print("-->"+str(selected_lap))
-        
     def populate_CB_laps(self, total_laps):
         self.cbLaps.clear()
         
         for i in range(total_laps + 1):
             self.cbLaps.addItem('Lap {}'.format(i))
+
+
+    def update_lap_selected(self, index):
+        self.selected_lap = index
+        
+        if self.selected_lap >= 0:
+            #print("->"+str(self.selected_lap))
+            currentLap_df = self.extract_csv_info_perLap(self.selected_lap, self.df)
             
+            currentLap_avg_speed = round(currentLap_df['speed'].mean() * 1.852, 2)
+            currentLap_avg_temp= round(currentLap_df['temperature'].mean(), 2)
+            
+            lap_time = currentLap_df.iloc[-1]['CurrentTime'] - currentLap_df.iloc[0]['CurrentTime']
+            lap_time_seconds = lap_time.total_seconds()
+            lap_time_str = '{:02d}:{:02d}:{:02d}'.format(int(lap_time_seconds // 3600), int(lap_time_seconds % 3600 // 60), int(lap_time_seconds % 60))
 
-    def racingLineMap_plot(self):
-        pass
-
-    def circuitElevation_plot(self):
-        self.racingLineMap_button.setEnabled(True)
+            self.info_label_lapTime.setText("LapTime: " + lap_time_str)
+            self.info_label_lapTime.setEnabled(True)
+            self.info_label_speed_per_lap.setText("Avg Speed: " + str(currentLap_avg_speed) + " km/h")
+            self.info_label_speed_per_lap.setEnabled(True)
+            self.info_label_temp_per_lap.setText("Avg Temp: " + str(currentLap_avg_temp) + " ºC")
+            self.info_label_temp_per_lap.setEnabled(True)
+        
+            
+    def extract_csv_info_perLap(self, lap_selected, dataFrame):
+        df_currentLap = dataFrame.loc[dataFrame['currentLap'] == lap_selected]
+        return df_currentLap
+    
 
     def speed_plot(self):
-        pass
+        self.speed_GraphWindow = speedGraph(self.df)
+        self.speed_GraphWindow.show()
+        
 
     def accelerometer_plot(self):
-        pass
+        print('accelerometer')
 
     def temperature_plot(self):
-        pass
+        print('temperature')
+
+    def racingLineMap_plot(self):
+        print('rancing line')
+
+    def circuitElevation_plot(self):
+        self.circuitElevation_GraphWindow = circuitElevationGraph(self.extract_csv_info_perLap(self.selected_lap, self.df))
+        self.circuitElevation_GraphWindow.show()
+    
+    def speed_perLap_plot(self):
+        self.speedPerLap_GraphWindow = speedGraph(self.extract_csv_info_perLap(self.selected_lap, self.df))
+        self.speedPerLap_GraphWindow.show()
+
+    def accelerometer_perLap_plot(self):
+        print('accelerometer per lap')
+    
+    def temp_perLap_plot(self):
+        print('temp per lap')
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyleSheet("QLabel{font-size: 11pt;} QPushButton{font-size: 10pt;} QComboBox{font-size: 10pt;}")
-    #app.setStyleSheet("QPushButton{font-size: 10pt;}")
-
+    #app.setStyleSheet("QLabel{font-size: 11pt;} QPushButton{font-size: 10pt;} QComboBox{font-size: 10pt;}")
+    app.setStyleSheet("QLabel{font-size: 9pt;} QPushButton{font-size: 9pt;} QComboBox{font-size: 9pt;}")
     w = MainWindow()
     w.show()
     app.exec()
