609e079ff90e37947d5b0b6125572e8eebeba4aa
SoleR7 <jordisolersierra@gmail.com>
2023-05-28 22:52:54 +0200

    telemetry logs folder added

--- Desktop App/ECO-Marathon_TelemetryAppV2.py
+++ Desktop App/ECO-Marathon_TelemetryAppV2.py
@@ -382,7 +382,7 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setFixedSize(850, 1020)
+        self.setFixedSize(750, 800)
         self.csv_file_name = None
         self.selected_lapA = 0
         self.selected_lapB = 0
@@ -406,7 +406,7 @@ class MainWindow(QMainWindow):
         # logo
         self.logo_label = QLabel(self)
         pixmap = QPixmap('logo_equipo.png')
-        pixmap = pixmap.scaled(850, 1020, Qt.KeepAspectRatio)
+        pixmap = pixmap.scaled(750, 800, Qt.KeepAspectRatio)
         self.logo_label.setPixmap(pixmap)
         self.logo_label.setGeometry(0, 0, pixmap.width(), pixmap.height())
 
